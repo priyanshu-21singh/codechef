cf264fe68d0d0bd791a45bb76dbe082fca2559c7
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-02-29 12:51:08 +0530

    Add files via upload

---
+++ Algomaniac Finals.py
@@ -0,0 +1,11 @@
+# Input
+day_shreyan_can_go = int(input().strip())
+
+# Algomaniac Finals date
+algomaniac_finals_date = 17
+
+# Check if Shreyan can attend
+if day_shreyan_can_go == algomaniac_finals_date:
+    print("YAY")
+else:
+    print("NO")
