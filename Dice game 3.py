fe7929a52aaefe7430a14fd1edbd96b999afd5d1
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-03-21 02:51:50 +0530

    Add files via upload

---
+++ Dice game 3.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+
+for _ in range(t):
+    
+    n = int(input())
+    
+    
+    if n == 1:
+        print("6")
+    elif n % 2 == 0:
+        w = n // 2
+        print(13 * w)
+    else:
+        w = n // 2
+        print(13 * w + 6)
