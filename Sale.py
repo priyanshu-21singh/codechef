4efaa077ead42a1b943d1ecae077c30f1c476a16
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-03-21 02:52:16 +0530

    Add files via upload

---
+++ Sale.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    sum_sales = sum(arr)
+    sum_sales += arr[-1]
+    max_sales = sum_sales
+    
+    for j in range(n - 2, -1, -1):
+        sum_sales = sum_sales + arr[j] - 2 * arr[j + 1]
+        if sum_sales >= max_sales:
+            max_sales = sum_sales
+    
+    print(max_sales)
