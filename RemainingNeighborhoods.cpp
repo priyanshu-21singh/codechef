c6fcdba44c58173d6870f5f76357827c2c33d0ef
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-04-24 20:34:42 +0530

    Add files via upload

---
+++ RemainingNeighborhoods.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    
+    int N;
+    std::cin >> N;
+
+    
+    const int totalNeighborhoods = 100;
+
+    
+    int remainingNeighborhoods = totalNeighborhoods - N;
+
+    
+    std::cout << remainingNeighborhoods << std::endl;
+
+    return 0;
+}
