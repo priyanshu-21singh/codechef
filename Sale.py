e64f54d24508c6ae5b6740b3335b1e5f37c1dd3c
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-06-22 15:17:25 +0530

    Update Sale.py

--- Sale.py
+++ Sale.py
@@ -6,6 +6,7 @@ for _ in range(t):
     sum_sales = sum(arr)
     sum_sales += arr[-1]
     max_sales = sum_sales
+    
     for j in range(n - 2, -1, -1):
         sum_sales = sum_sales + arr[j] - 2 * arr[j + 1]
         if sum_sales >= max_sales:
