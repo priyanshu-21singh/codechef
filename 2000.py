1d02974719ccf61d98c34283a4fd6b6ddf7a314b
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-07-07 15:23:44 +0530

    Update 2000.py

--- 2000.py
+++ 2000.py
@@ -3,7 +3,6 @@ def find_number_of_notes(n):
     total_amount = n * 2000
     # Calculate the number of Rs. 500 notes needed
     number_of_500_notes = total_amount // 500
-    
     return number_of_500_notes
 
 # Input the number of Rs. 2000 notes Chef has collected
