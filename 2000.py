26e233ebe4e0773f512bb83d06ecea42a4d0d74e
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-07-07 15:23:59 +0530

    Update 2000.py

--- 2000.py
+++ 2000.py
@@ -4,8 +4,7 @@ def find_number_of_notes(n):
     # Calculate the number of Rs. 500 notes needed
     number_of_500_notes = total_amount // 500
     return number_of_500_notes
-
-# Input the number of Rs. 2000 notes Chef has collected
+    # Input the number of Rs. 2000 notes Chef has collected
 n = int(input())
 
 # Output the number of Rs. 500 notes needed
