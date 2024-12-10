0bf1a02fc93630b368936cadd9325f0236d6c3ed
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-12-10 23:45:29 +0530

    Update 0and 1 matrix.py

--- 0and 1 matrix.py
+++ 0and 1 matrix.py
@@ -3,7 +3,7 @@ while(i<rows and j>=0):
   while (m[i][j]==1)
 j-=1
 row = i
-i+=1  outer loop will start irr...
+i+=1  outer loop will start irr...                
 #loop  will break 
 return i
      if no 1 is present in any rows
